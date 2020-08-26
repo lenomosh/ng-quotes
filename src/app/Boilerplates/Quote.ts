cb0ca313f3efd6dcffaee096de9ac3d3ee148b4c
Lennox <omondilenox@outlook.com>
2020-08-26 10:43:33 +0300

    refactored some code

---
+++ src/app/Boilerplates/Quote.ts
@@ -0,0 +1,16 @@
+import * as moment from 'moment';
+export class Quotes {
+  public upvotes: number;
+  public downvotes: number;
+
+  constructor(
+    public username: string,
+    public quote: string,
+    public author: string,
+    public datecreated: moment.Moment
+  ){
+    this.upvotes = 0;
+    this.downvotes = 0;
+
+  }
+}
