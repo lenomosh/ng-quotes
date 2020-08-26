85424ed6a83effe1f72adf79c62f09aec66dd1c9
Lennox <omondilenox@outlook.com>
2020-08-26 10:57:05 +0300

    formatted quotes add component

--- src/app/quotes-add/quotes-add.component.ts
+++ src/app/quotes-add/quotes-add.component.ts
@@ -1,31 +1,32 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import {Quotes} from '../Boilerplates/Quote';
+import { Quotes } from '../Boilerplates/Quote';
 import * as moment from 'moment';
 
 @Component({
   selector: 'app-quotes-add',
   templateUrl: './quotes-add.component.html',
-  styleUrls: ['./quotes-add.component.scss']
+  styleUrls: ['./quotes-add.component.scss'],
 })
 export class QuotesAddComponent implements OnInit {
-
-  constructor() { }
-  // newQuote = new Quotes ('', '', '', moment());
+  constructor() {}
   @Output() emitQuote = new EventEmitter();
   quote: string;
   username: string;
   author: string;
   newQuote: any;
 
-  submitQuote( ): void{
-    this.newQuote = new Quotes(this.username,  this.quote, this.author, moment());
+  submitQuote(): void {
+    this.newQuote = new Quotes(
+      this.username,
+      this.quote,
+      this.author,
+      moment()
+    );
     this.quote = '';
     this.author = '';
     this.username = '';
     this.emitQuote.emit(this.newQuote);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
