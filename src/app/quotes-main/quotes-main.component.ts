036abf40748d920bd06b0486fefaa41e0dd7e016
Lennox <omondilenox@outlook.com>
2020-08-26 10:50:23 +0300

    indented quote main ts

--- src/app/quotes-main/quotes-main.component.ts
+++ src/app/quotes-main/quotes-main.component.ts
@@ -1,33 +1,46 @@
-import {Component, OnInit} from '@angular/core';
-import {Quotes} from '../Boilerplates/Quote';
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../Boilerplates/Quote';
 import * as moment from 'moment';
 
 @Component({
   selector: 'app-quotes-main',
   templateUrl: './quotes-main.component.html',
-  styleUrls: ['./quotes-main.component.scss']
+  styleUrls: ['./quotes-main.component.scss'],
 })
 export class QuotesMainComponent implements OnInit {
-  quotesObj: {quote: string, author: string}[] = [
+  quotesObj: { quote: string; author: string }[] = [
     {
-      quote: 'Life isn’t about getting and having, it’s about giving and being.', author: 'Kevin Kruse'
+      quote:
+        'Life isn’t about getting and having, it’s about giving and being.',
+      author: 'Kevin Kruse',
     },
     {
-      quote: 'Whatever the mind of man can conceive and believe, it can achieve.', author: 'Napoleon Hill'},
+      quote:
+        'Whatever the mind of man can conceive and believe, it can achieve.',
+      author: 'Napoleon Hill',
+    },
     {
-      quote: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+      quote: 'Strive not to be a success, but rather to be of value.',
+      author: 'Albert Einstein',
+    },
     {
-      quote: 'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+      quote:
+        'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.',
+      author: 'Robert Frost',
+    },
     {
-      quote: 'I attribute my success to this: I never gave or took any excuse.', author: 'Florence Nightingale'},
+      quote: 'I attribute my success to this: I never gave or took any excuse.',
+      author: 'Florence Nightingale',
+    },
     {
-      quote: 'You miss 100% of the shots you don’t take.', author: 'Wayne Gretzky'
-    }
+      quote: 'You miss 100% of the shots you don’t take.',
+      author: 'Wayne Gretzky',
+    },
   ];
   quotes: Quotes[] = [];
   constructor() {
-    this.quotesObj.map(
-      element => this.quotes.push(
+    this.quotesObj.map((element) =>
+      this.quotes.push(
         new Quotes(
           'Lennox O.',
           element.quote,
@@ -40,32 +53,30 @@ export class QuotesMainComponent implements OnInit {
   currentScore: number;
   newScore: number;
   counter: number;
-  votesOnlyArr: (number)[] = [];
-  addNewQuote(emittedQuote): void{
+  votesOnlyArr: number[] = [];
+  addNewQuote(emittedQuote): void {
     this.quotes.push(emittedQuote);
   }
 
-  upvote(i): void{
-    this.quotes[i].upvotes -= - 1;
+  upvote(i): void {
+    this.quotes[i].upvotes -= -1;
   }
-  downvote(i): void{
-    this.quotes[i].downvotes -= - 1;
+  downvote(i): void {
+    this.quotes[i].downvotes -= -1;
   }
-  delQuote(i): void{
+  delQuote(i): void {
     this.quotes.splice(i, 1);
   }
   highestUpvote(): number {
     Math.max.apply(null, this.votesOnlyArr);
     this.currentScore = 0;
     this.newScore = 0;
-    this.quotes.map(quote => {
-      const {upvotes} = quote;
+    this.quotes.map((quote) => {
+      const { upvotes } = quote;
       return this.votesOnlyArr.push(upvotes);
     });
     return Math.max(...this.votesOnlyArr);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
