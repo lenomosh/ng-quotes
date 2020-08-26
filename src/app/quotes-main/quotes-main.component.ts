d68f721e4ab7f65b8ab867a86d94b816b7b4b2f9
Lennox <omondilenox@outlook.com>
2020-08-26 09:39:02 +0300

    modified app html

--- src/app/quotes-main/quotes-main.component.ts
+++ src/app/quotes-main/quotes-main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Quotes} from '../Boilerplates/Quote';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-quotes-main',
@@ -6,8 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-main.component.scss']
 })
 export class QuotesMainComponent implements OnInit {
+  quotesObj: {quote: string, author: string}[] = [
+    {
+      quote: 'Life isn’t about getting and having, it’s about giving and being.', author: 'Kevin Kruse'
+    },
+    {
+      quote: 'Whatever the mind of man can conceive and believe, it can achieve.', author: 'Napoleon Hill'},
+    {
+      quote: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+    {
+      quote: 'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+    {
+      quote: 'I attribute my success to this: I never gave or took any excuse.', author: 'Florence Nightingale'},
+    {
+      quote: 'You miss 100% of the shots you don’t take.', author: 'Wayne Gretzky'
+    }
+  ];
+  quotes: Quotes[] = [];
+  constructor() {
+    this.quotes.map(
+      element => this.quotes.push(
+        new Quotes(
+          'Lennox O.',
+          element.quote,
+          element.author,
+          moment('2008-10-10')
+        )
+      )
+    );
+  }
+  currentScore: number;
+  newScore: number;
+  counter: number;
 
-  constructor() { }
+  addNewQuote(emittedQuote): void{
+    this.quotes.push(emittedQuote);
+  }
+
+  upvote(i): void{
+    this.quotes[i].upvotes -= - 1;
+  }
+  downvote(i): void{
+    this.quotes[i].downvotes -= - 1;
+  }
+  delQuote(i): void{
+    this.quotes.splice(i, 1);
+  }
+  highestUpvote(): number {
+    this.currentScore = 0;
+    this.newScore = 0;
+    this.quotes.map(quote => {
+      this.newScore = quote.upvotes;
+      this.currentScore = this.newScore > this.currentScore ? this.newScore : this.currentScore;
+    }
+    );
+    return this.currentScore;
+  }
 
   ngOnInit(): void {
   }
