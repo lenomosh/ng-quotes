7756ca1329bb799fdf213fb0e35184f4dca5e6ef
Lennox <omondilenox@outlook.com>
2020-08-26 10:55:49 +0300

    formatted date difference.ts

--- src/app/Pipes/DateDifference/date-difference.pipe.ts
+++ src/app/Pipes/DateDifference/date-difference.pipe.ts
@@ -1,13 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 @Pipe({
-  name: 'dateDifference'
+  name: 'dateDifference',
 })
 export class DateDifferencePipe implements PipeTransform {
-
   transform(value: moment.Moment, ...args: unknown[]): unknown {
     const now = moment();
     return now.diff(value, 'days');
   }
-
 }
