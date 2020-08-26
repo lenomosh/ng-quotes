a0b0bed91f99da0d87e2ad698fdd823b2e7ed966
Lennox <omondilenox@outlook.com>
2020-08-26 10:58:07 +0300

    formatted quote details component

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,16 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Quotes} from '../Boilerplates/Quote';
+import { Component, Input, OnInit } from '@angular/core';
+import { Quotes } from '../Boilerplates/Quote';
 
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
-  styleUrls: ['./quotes-details.component.scss']
+  styleUrls: ['./quotes-details.component.scss'],
 })
 export class QuotesDetailsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
   @Input() quote: Quotes;
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
