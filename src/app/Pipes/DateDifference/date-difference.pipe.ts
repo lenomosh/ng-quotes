81692343db4b92f19c5385a0bd5999b65da5cfc8
Lennox <omondilenox@outlook.com>
2020-08-26 08:53:36 +0300

    modified quote details + datedifference pipe

--- src/app/Pipes/DateDifference/date-difference.pipe.ts
+++ src/app/Pipes/DateDifference/date-difference.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment'
+import * as moment from 'moment';
 @Pipe({
   name: 'dateDifference'
 })
 export class DateDifferencePipe implements PipeTransform {
 
-  transform(value: Date, ...args: unknown[]): unknown {
+  transform(value: moment.Moment, ...args: unknown[]): unknown {
     const now = moment();
     return now.diff(value, 'days');
   }
--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Quotes} from '../Boilerplates/Quote';
 
 @Component({
   selector: 'app-quotes-details',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class QuotesDetailsComponent implements OnInit {
 
   constructor() { }
-
+  @Input() quote: Quotes;
   ngOnInit(): void {
   }
 
